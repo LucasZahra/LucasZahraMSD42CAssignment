0b29cebd5b31b874a0df76910fa6ecc415d424ff
LucasZahra <73173365+LucasZahra@users.noreply.github.com>
2021-01-06 21:00:09 +0100

    Background,Shredder,Player, Created all folders, paths
    Finished scroll background with good speed.
    Finished Shredder
    Player moves with arrow keys.
    Made all the folders in the assets.
    Started doing the paths with scripts.

---
+++ 2DCarGame/Assets/Scripts/Player.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    float xMin, xMax, yMin, yMax;
+
+    [SerializeField] float moveSpeed = 7f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetUpMoveBoundaries();
+    }
+
+    private void SetUpMoveBoundaries()
+    {
+        Camera gameCamera = Camera.main;
+
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, (float)0.5, 0)).x;
+
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, (float)0.1, 0)).y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        var newXPos = transform.position.x + deltaX;
+        newXPos = Mathf.Clamp(newXPos, xMin, xMax);
+
+        var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        var newYPos = transform.position.y + deltaY;
+        newYPos = Mathf.Clamp(newYPos, yMin, yMax);
+
+        this.transform.position = new Vector2(newXPos, newYPos);
+    }
+}
---
+++ 2DCarGame/Assets/Scripts/Shredder.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shredder : MonoBehaviour
+{
+    private void onTriggerEnter2D(Collider2D otherObject)
+    {
+        Destroy(otherObject.gameObject);
+    }
+}
