1e6290f44ada3714e6754ad7b4247225008e986d
LucasZahra <73173365+LucasZahra@users.noreply.github.com>
2021-01-06 21:57:08 +0100

    Take Damage, player gets destroyed
    The player can now take damage with a health of 500, if its health goes to 0 the player is destoryed.

---
+++ 2DCarGame/Assets/Scripts/Health.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+
+    [SerializeField] float health = 500f;
+
+    private void OnTriggerEnter2D(Collider2D otherObject)
+    {
+        DamageDealer dmgDealer = otherObject.gameObject.GetComponent<DamageDealer>();
+
+        health -= dmgDealer.GetDamage();
+
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
