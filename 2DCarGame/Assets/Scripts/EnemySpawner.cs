8e97baf525f8455edb6e5fa4dba42aa370922563
LucasZahra <73173365+LucasZahra@users.noreply.github.com>
2021-01-06 21:39:16 +0100

    Wave + Enemy Spawner
    Made the EnemySpawner script to spawn enemies into the waves.

---
+++ 2DCarGame/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+
+    [SerializeField] bool looping = false;
+    [SerializeField] List<WaveConfig> WaveConfigList;
+
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        do
+        {
+            yield return StartCoroutine(SpawnAllWaves());
+        }
+        while (looping);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private IEnumerator SpawnAllEnemiesInWave(WaveConfig waveConfig)
+    {
+        for (int enemyCount = 0; enemyCount < waveConfig.GetNumberOfEnemies(); enemyCount++)
+        {
+            var newEnemy = Instantiate(
+                waveConfig.GetObstaclePrefab(),
+                waveConfig.GetWaypoints()[0].transform.position,
+                Quaternion.identity);
+
+            newEnemy.GetComponent<EnemyPathing>().SetWaveConfig(waveConfig);
+
+            yield return new WaitForSeconds(waveConfig.GetTimeBetweenSpawns());
+        }
+    }
+
+    private IEnumerator SpawnAllWaves()
+    {
+        foreach (WaveConfig currentwave in WaveConfigList)
+        {
+            yield return StartCoroutine(SpawnAllEnemiesInWave(currentwave));
+        }
+    }
+}
